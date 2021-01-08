cee5c56bd75314d8b68c30668c252d2e97ed1f9d
h-shiraishi0402 <hiro19900402@gmail.com>
2021-01-08 22:45:01 +0900

    Initial Commit

---
+++ TiktokStyleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TiktokStyleApp
+//
+//  Created by 白石裕幸 on 2021/01/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
